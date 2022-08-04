9af97e0cb71e34a3f887ff1b781c88e92dba0b42
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-08-03 22:15:18 -0700

    refractored inquirer

---
+++ index.js
@@ -0,0 +1,101 @@
+// packages needed
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generatePage = require('./utils/generatePage')
+const { nextTick } = require('process')
+
+
+const name =
+{
+    type: 'input',
+    name: 'name',
+    message: "What's the person's name?",
+}
+
+const ID =
+{
+    type: 'input',
+    name: 'id',
+    message: "What's the person's ID?",
+}
+
+const email =
+{
+    type: 'input',
+    name: 'email',
+    message: "What is the person's email address?",
+}
+
+const nextAction = 
+{
+    type: 'list',
+    name: 'addOrFinish',
+    message: 'What would you like to do next?',
+    choices: [
+        'Add an engineer',
+        'Add an intern',
+        'Finish building the team'
+    ]
+}
+
+// create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your team manager?',
+    },
+
+    ID,
+    email,
+
+    {
+        type: 'input',
+        name: 'officeNum',
+        message: "What is the manager's office number?",
+    },
+
+    nextAction
+]
+
+
+function getAnswers() {
+    return inquirer.prompt(questions).then(answer => {
+        switch(answer.addOrFinish) {
+
+            case 'Add an engineer':
+                inquirer.prompt([
+                    name,
+                    ID,
+                    email,
+                    {
+                        type: 'input',
+                        name: 'github',
+                        message: "What's the engineer's github username?",
+                    },
+                    nextAction
+                ])
+                break
+
+            case 'Add an intern':
+                inquirer.prompt([
+                    name,
+                    ID,
+                    email,
+                
+                    {
+                        type: 'input',
+                        name: 'school',
+                        message: "What's the intern's school?",
+                    },
+                
+                    nextAction
+                ])
+                break
+            case 'Finish building the team':
+                // fs.writeFile()
+                    console.log(answer.officeNum)
+        }
+    })
+}
+getAnswers()
